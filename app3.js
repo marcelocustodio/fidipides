fc8f8f0861e866ec9dfa9951b9f1807e43dd85ec
marcelocustodio <marcelodoispontozero@gmail.com>
2022-08-11 15:09:57 -0400

    terceiro commit...

---
+++ app3.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+module.exports = app
+
+//import 
+
+var server = app.listen(777)
