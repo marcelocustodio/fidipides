b6add0726b9395e853fa34b97314da8545901d86
marcelocustodio <marcelodoispontozero@gmail.com>
2022-08-11 20:41:25 -0400

    oitavo commit...

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World, funciona por favor!');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'sql10.freemysqlhosting.net',
+  user     : 'sql10512351',
+  password : 'H84xeLLTMt',
+  database : 'tabela_do_fidipides'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM `tabela_do_fidipides`', function (error, results, fields) {
+  if (error) throw error;
+  // console.log('The solution is: ', results;
+  app.get('/', function (req, res) {
+   res.send('Hello World, funciona por favor!' + results);
 })
+});
+ 
+connection.end();
+
+
+
+//app.get('/', function (req, res) {
+//   res.send('Hello World, funciona por favor!');
+//})
 
 //module.exports = app
 
