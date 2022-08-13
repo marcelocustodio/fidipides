5dfefe0e5eb86ca282253e83f7e98fd25dc53c44
marcelocustodio <marcelodoispontozero@gmail.com>
2022-08-13 13:00:48 -0400

    novo commit 2

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-var express = require('express');
+import express from 'express';
+// var express = require ("express")
 
-var app = express();
+import banco from './banco.js'
+// let banco = require ('./banco.js')
 
-var mysql = require('mysql');
+console.log(banco)
 
-var connection = mysql.createConnection({
-  host     : 'sql10.freemysqlhosting.net',
-  user     : 'sql10512351',
-  password : 'H84xeLLTMt',
-  database : 'sql10512351'
-});
- 
-connection.connect();
- 
-var r = 5
-
-connection.query('SELECT * FROM `tabela_do_fidipides`', function (error, results, fields) {
-  if (error) throw error;
-  // console.log('The solution is: ', results;
-  r = results[0].nome
-});
- 
-connection.end();
-
+let app = express();
 
 app.get('/', function (req, res) {
-   res.send('Hello World, funciona por favor!' + r);
+   res.send('FIDIPIDES ' + banco);
 })
 
 //app.get('/', function (req, res) {
 //   res.send('Hello World, funciona por favor!');
 //})
-
-//module.exports = app
-
-//import 
 
 let port = process.env.PORT || 80
 
