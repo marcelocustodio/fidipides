155dfbd8c0cf07998090fe7a4c3376a0ff4b059b
marcelocustodio <marcelodoispontozero@gmail.com>
2022-08-13 16:40:41 -0400

    novo commit 3

--- index.js
+++ index.js
@@ -9,12 +9,36 @@ console.log(banco)
 let app = express();
 
 app.get('/', function (req, res) {
-   res.send('FIDIPIDES ' + banco);
+   res.send(
+    'FIDIPIDES : Sistema de Gerenciamento dos Atletas do TCE-AM em Competições<br>' +
+    '<a href="atletas">Atletas</a>'
+  );
 })
 
-//app.get('/', function (req, res) {
-//   res.send('Hello World, funciona por favor!');
-//})
+app.get('/atletas', function (req, res) {
+  var lista = ''
+  banco[0]['atletas'].forEach( (element, index) => {
+    lista += '<a href="/atleta/' + element['nome'] + '">' + element['nome_completo'] + '</a><br>'
+  });
+   res.send('Lista de Atletas<br>' + lista);
+})
+
+app.get('/atleta/:nome', function (req, res) {
+  var chave = 9
+  banco[0]['atletas'].forEach( (element, index) => {
+    if (element['nome'] === req.params.nome) {
+      console.log('Nome completo do cidadão: ' + element['nome_completo'])
+      chave = index
+    }
+  });
+  res.send(
+    'Passado: ' + req.params.nome + 
+    'Atleta: ' + banco[0]['atletas'][chave]['nome_completo'] + 
+    'Modalidades: ' +
+    'Participações HOJE: ' +
+    'Participações TOTAIS: '
+  );
+})
 
 let port = process.env.PORT || 80
 
