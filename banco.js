93a00cc6166e600c5c571eb0076c84dbbdb84dcf
marcelocustodio <marcelodoispontozero@gmail.com>
2022-08-13 13:00:07 -0400

    novo commit

---
+++ banco.js
@@ -0,0 +1,15 @@
+let banco = [
+    {
+        'atletas':[
+            {'nome':'Marcelo Monteiro Custódio', 'sexo':'m'}
+        ]
+    },
+    {
+        'modalides':[
+            {}
+        ]
+    },
+    {}
+]
+
+export default banco
