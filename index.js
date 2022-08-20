4a3a80e2e43ce03c708808c15278c883c6ff8d5c
marcelocustodio <marcelodoispontozero@gmail.com>
2022-08-20 12:59:58 -0400

    20.08 -- 07

--- index.js
+++ index.js
@@ -176,6 +176,11 @@ let cabecalho = () => {
   '  }' +
   '}' +
   '</style>' +*/
+  '  <!-- Required meta tags -->' +
+  '  <meta charset="utf-8">' +
+  '  <meta name="viewport" content="width=device-width, initial-scale=1">' +
+  '  <!-- Bootstrap CSS -->' +
+  '  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">' +
   '</head>' +
   '<body>' +
   'FIDIPIDES v. 0.3.6: Sistema de Gerenciamento dos Atletas do TCE-AM em Competições<br><br>' +
